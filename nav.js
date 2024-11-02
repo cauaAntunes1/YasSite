6b87ac16f334e362ba00b47a1ae699af1e477dfa
cauaAntunes1 <134733625+cauaAntunes1@users.noreply.github.com>
2024-11-02 18:24:34 -0300

    Delete nav.js

--- nav.js
+++
@@ -1,6 +0,0 @@
-const ele = document.createElement("div")
-ele.className = "nav-container"
-ele.innerHTML = `<header class="nav-header"><a class="nav-header_title" href="">Yas Site</a><nav class="user-nav"><a class="user-nav_icon-box" href="home.html">home</a><a class="user-nav_icon-box" href="explore.html">explore</a><a class="user-nav_icon-box" href="signup.html">sign-up</a><a class="user-nav_icon-box" href="submit.html">submit</a><a class="user-nav_icon-box" href="help.html">help</a></nav></header>`
-
-var rootDiv = document.body.getElementsByClassName("root")
-rootDiv[0].appendChild(ele)
