61fcd60547ef257a49ca7ef1d9f4116a02297761
cauaAntunes1 <134733625+cauaAntunes1@users.noreply.github.com>
2024-11-03 22:50:28 -0300

    Add files via upload

--- scripts/downloadFile.js
+++ scripts/downloadFile.js
@@ -2,11 +2,22 @@ var addFileElement = document.getElementById("download-file_add")
 var removeFileElement = document.getElementById("download-file_minus")
 
 var filesElements = document.getElementById("download-files_content")
+var genresElements = document.getElementById("game-genres")
+
+var genreInput = document.getElementById("game-genre_input")
+var genreButton = document.getElementById("game-genre_button")
+
+var genresNum = 0
 
 function addFile() {
     const newEle = document.createElement("td")
     newEle.className = "download-file"
-    newEle.innerHTML = `<div class="download-file_name"><p>File name</p><input placeholder="Insert file name" type="text">    </div><div class="download-file_file"><p>File</p><input type="file" name="gamefile" id="gamefile"></div>`
+    newEle.innerHTML = `<div class="download-file_name">
+    <p>File name</p><input placeholder="Insert file name" type="text">
+    </div>
+    <div class="download-file_file">
+    <p>File</p><input type="file" name="gamefile" id="gamefile">
+    </div>`
     filesElements.rows[0].appendChild(newEle)
     document.documentElement.scrollTop += 274
 }
@@ -17,5 +28,24 @@ function removeFile() {
     }
 }
 
+
+function removeGenre(clickedId) {
+    const selButton = document.getElementById(clickedId)
+    selButton.parentElement.remove()
+}
+
+function addGenre() {
+    if (genreInput.value != "") {
+        const newEle = document.createElement("td")
+        newEle.className = "game-genres_content"
+        newEle.innerHTML = `<button onClick="removeGenre(this.id)" id="${genresNum}">X</button>
+                            <p>${genreInput.value}</p>`
+        genresElements.rows[0].appendChild(newEle)
+        genresNum++
+    }
+}
+
+
 addFileElement.addEventListener('click', addFile)
 removeFileElement.addEventListener('click', removeFile)
+genreButton.addEventListener('click', addGenre)
